e5d613b3640cfae1a698420ae052762641d03dec
kamrul islam <mdkamrulislam9810@gmail.com>
2023-07-08 10:59:20 -0700

    'fb-timeline'

---
+++ src/data.js
@@ -0,0 +1,135 @@
+
+const postsData = [
+    {
+        id: 1,
+        content: `Here are some key areas where AI is used in 𝐌𝐚𝐫𝐤𝐞𝐭𝐢𝐧𝐠
+        𝐃𝐚𝐭𝐚 𝐀𝐧𝐚𝐥𝐲𝐬𝐢𝐬 𝐚𝐧𝐝 𝐈𝐧𝐬𝐢𝐠𝐡𝐭𝐬:AI - powered algorithms can process large volumes of data quickly and extract valuable insights.Marketers can use AI to analyze customer behavior, preferences, and trends, enabling them to make data- driven decisions and develop effective marketing strategies`,
+        image: 'https://scontent.fcla5-1.fna.fbcdn.net/v/t39.30808-6/358657400_318159034053857_9179683267492470329_n.jpg?stp=dst-jpg_p180x540&_nc_cat=110&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeEZqEAB2grbpg2tJp_ee1WnSwUv1yOnMLFLBS_XI6cwsYwP-DVhkBhpiEEamPqYyIEreTBI62DJRVVEyxG41vOS&_nc_ohc=LGzyeUR6S2sAX-rtTV9&_nc_zt=23&_nc_ht=scontent.fcla5-1.fna&oh=00_AfDhDAAQOltrNq01SbFzlGASq0OlvCzjMBLF-64jpTSi6g&oe=64AD5E8B',
+        time: "55 min",
+        like: '500',
+        comment: '200',
+        author: {
+            name: 'Google AI',
+            image: "https://scontent.fcla5-1.fna.fbcdn.net/v/t39.30808-1/358596960_318160747387019_775796297942667343_n.jpg?stp=c16.0.40.40a_cp0_dst-jpg_p40x40&_nc_cat=108&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeGULMbi9-Xp_ESO98IN_l_R45GO7fJvxbjjkY7t8m_FuPgS89NcMtjYPt4ahnhr5_2U8lXJRQmOspkg7kIwOPVo&_nc_ohc=IBqsjhIl-DcAX_bhKd3&_nc_ht=scontent.fcla5-1.fna&oh=00_AfBbGrI7iJdzFxPCFMziYVzUdRj1BNm4E6VR9xU-pt7Opw&oe=64ACD43B"
+        }
+    },
+    {
+        id: 2,
+        content: `রোবটিক্স কুইজের সাপ্তাহিক প্র্যাকটিস ২০
+        -------------------------------------------------
+        বাংলাদেশ রোবট অলিম্পিয়াডের জাতীয় পর্বের একটি নিয়মিত ক্যাটাগরি - রোবটিক্স কুইজ। এই ক্যাটাগরিতে রোবটিক্স সম্পর্কিত বিভিন্ন টেকনিক্যাল প্রশ্ন, গাণিতিক সমস্যা ইত্যাদি থাকে। তবে  নিজে নিজে প্র্যাকটিস করে এই ক্যাটাগরিতে প্রস্তুত হওয়া সম্ভব। 
+         প্রতি সপ্তাহের বৃহস্পতিবার রাতে একটি করে নমুনা প্রশ্ন`,
+        image: 'https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-6/356665122_648008230681586_8453818034624933275_n.jpg?stp=dst-jpg_s600x600&_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeHz6ujECREWN9Kx_xijKPWPffAuTek253598C5N6TbnftKJ7hJYYZBqV7xs5JAXYZvWY6dX-L-rKAowMEDQ8n-S&_nc_ohc=sBFCKb_Yjf8AX9s3MwC&_nc_zt=23&_nc_ht=scontent.fcla5-2.fna&oh=00_AfBCWbfC-SVTkJyDP36VTAT79g-ef1ufxMiIUFVUiYVXrA&oe=64ABBF40',
+        time: "45 min",
+        like: '100',
+        comment: '52',
+        author: {
+            name: 'Bangladesh Robot Olympiad : BDRO',
+            image: "https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-1/335446439_630301588907133_2471131459273644185_n.jpg?stp=c191.30.1666.1667a_cp0_dst-jpg_s40x40&_nc_cat=100&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeEo5fn2NYr1LQ9V87T4_guDwJ0xD-DPjQ_AnTEP4M-NDxCOYHckxYwGfwREifIK7j5vg64px4uIETHX2_sEcQ10&_nc_ohc=we2Qz8jO3gYAX8tyw7B&_nc_oc=AQn73YJAPIKJ2a_bwHGG9i4o1wRUAhWmaE3P80xbkHW65i-52A1SNjSxqfeD0__6OKc&_nc_ht=scontent.fcla5-2.fna&oh=00_AfCnTYDbYWE8PBAv6yC6b89J4_j1WBe6KndlfIlt5VD1MQ&oe=64AC6357"
+        }
+    },
+    {
+        id: 3,
+        content: `ইন্টারন্যাশনাল ট্রান্সফার মার্কেটের তথ্যানুযায়ী,  বাংলাদেশের বাজপাখি আনিসুর রহমান জিকোর বর্তমান মার্কেট ভ্যালু ২'২৫'০০০ ইউরো। 🇧🇩
+        সময় এসেছে গর্ব করার ❤️❤️`,
+        image: 'https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-6/357482913_589526593362322_6606370908860694085_n.jpg?stp=dst-jpg_p526x296&_nc_cat=100&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeFIqNrNFNdZv2RyBvlTteIXR0pIzrbPWeNHSkjOts9Z435Qt4g2Imasu1sDa84n8wWiK63O5uGGvHheXyQUSRNY&_nc_ohc=hKhTjNb-TDsAX-uoJVG&_nc_zt=23&_nc_ht=scontent.fcla5-2.fna&oh=00_AfB5_7KwihtrttK0rpGqiU8dmXhh94w6B8rF_K-jBji_nA&oe=64AC12A5',
+        time: "40 min",
+        like: '500',
+        comment: '20',
+        author: {
+            name: 'Footbal club ',
+            image: "https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-6/347576331_491964596396753_870247116345917754_n.jpg?stp=c8.0.50.50a_cp0_dst-jpg_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=70495d&_nc_eui2=AeH3dOBhkPtlrbPNbzaL54rViibF8p9lF5aKJsXyn2UXlq1UtooRFUIzNYY6RX3AoA1ujf1ipL3nYNMRG_--Nkz3&_nc_ohc=ddCcbbyKXusAX8c8I4L&_nc_zt=23&_nc_ht=scontent.fcla5-2.fna&oh=00_AfAa3NRsos44xrvWUAmtVBsU7aBq-dTOZ_-PYspOxrV7Jg&oe=64AC9B0C"
+        }
+    },
+    {
+        id: 4,
+        content: `আবার কি হইছে তোমার?`,
+        image: 'https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-6/358685936_660960976070156_572373129707709698_n.jpg?stp=dst-jpg_p526x296&_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFwEolhTghw6k8UHHv-wEH19u2Ih8j90bj27YiHyP3RuOcxPPPPInQokYCWKYYscAjIFdYqE-_B7mBrKp2BjYSW&_nc_ohc=9IhgMIktjDQAX9TFwtj&_nc_zt=23&_nc_ht=scontent.fcla5-2.fna&oh=00_AfDqscwJJ79kDwKxT6bEbrDwV8V5BYlPU9VKtFAUIjxlvQ&oe=64AD5D1D',
+        time: "38 min",
+        like: '100',
+        comment: '21',
+        author: {
+            name: '𝐒𝐢𝐧𝐠𝐥𝐞-পোলাপাইন',
+            image: "https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-1/307554688_459149856251270_9008376304180462025_n.jpg?stp=cp0_dst-jpg_p40x40&_nc_cat=1&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeH4n-9v7lwaYYU2j06mG2E8590GuufpUoTn3Qa65-lShPaUpXryimRN78JU_YX1bB90qP-MV5IZnENBrrYBFeEb&_nc_ohc=romPCEenruAAX8u3LoN&_nc_ht=scontent.fcla5-2.fna&oh=00_AfB1rLku6pzpYdTcMPrD8Wn5cimE9i1YK-1n_dGjNyYogQ&oe=64AD0F83"
+        }
+    },
+    {
+        id: 5,
+        content: `তামিম ইকবাল গণভবনে প্রধানমন্ত্রীর সাথে দেখা করতে গিয়েছে।🤝❤️
+        © ক্রিকফ্রেঞ্জির রিপোর্ট`,
+        image: 'https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-6/358620264_820394533040459_3407157097439455959_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeGm1ht7Nqt_BgWp8HBw27VS4dzllwFxQWDh3OWXAXFBYJcfselsb-jCgBhgE3d-xf6sBu312rbCrp4n4dtcJpFH&_nc_ohc=3g5U-MLZKEAAX_yA--U&_nc_zt=23&_nc_ht=scontent.fcla5-2.fna&oh=00_AfABDmrN4MdJHkdsiteGf83q80Fqep_qWSIlNxI9q53OmA&oe=64AD8786',
+        time: "35 min",
+        like: '1k',
+        comment: '500',
+        author: {
+            name: 'মায়া',
+            image: "https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-1/312466635_659827915763789_4249889523889090704_n.jpg?stp=cp0_dst-jpg_p40x40&_nc_cat=1&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeF_9xB5O4YQOHVQnZh45Rb-wEMEcWSvcxPAQwRxZK9zExQ-U4E5RT3j2LVDNk7YSi4uI458EzCyDoY0aMyCbHJ_&_nc_ohc=WQBf9Q9hP2AAX-4JP4P&_nc_ht=scontent.fcla5-2.fna&oh=00_AfCjVxzDRsTDMXRk0ui8rgzrbpwU8asdILMxiaxFSS13iw&oe=64ACD357"
+        }
+    },
+    {
+        id: 6,
+        content: `মাদকদ্রব্য নিয়ন্ত্রণ অধিদপ্তর
+        পদ সংখ্যা- ৩১৩ ( সিপাই)
+        Apply:  http://dnc.teletalk.com.bd/
+        আবেদন শুরুঃ ১০ জুলাই ২০২৩`,
+        image: 'https://scontent.fcla5-1.fna.fbcdn.net/v/t39.30808-6/358099745_288792553713540_4576749192301245146_n.jpg?stp=dst-jpg_p526x296&_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeFNpFNOi0CyiLWbIB0Q9rWjDUFeTXRL-WsNQV5NdEv5a37iOusjEVfZXMzkADbSgJecu9nHswTphyZ_EQEyp44g&_nc_ohc=nXN-r6p-piMAX_JL-Ap&_nc_zt=23&_nc_ht=scontent.fcla5-1.fna&oh=00_AfCHuoa0RGY4fkCNmzcGdmvjDdwGHw8amMrIJv_5baj6nA&oe=64AD6A00',
+        time: "30 min",
+        like: '200',
+        comment: '10',
+        author: {
+            name: 'বিসিএস স্পেশাল আপডেট',
+            image: "https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-1/349353252_765931675074650_5105859700391960476_n.jpg?stp=cp0_dst-jpg_p40x40&_nc_cat=102&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeFYRARjBNdkjMaBaEPrmS7RE-Mlj7lyE_8T4yWPuXIT_2cgI0Rm2F9OKZ4U27KPUMdMKrsMMSB3uYnsvgHOxSHD&_nc_ohc=u1cjAsXhnZYAX82K0NH&_nc_ht=scontent.fcla5-2.fna&oh=00_AfBm_xp9yZBZfpM_dZhssib1L9vLjpjtIPsvd9A8PSYFOA&oe=64ACF548"
+        }
+    },
+    {
+        id: 7,
+        content: `Flutter কি?
+        Flutter হল একটি একক কোডবেস, নেটিভলি কম্পাইল করা, মাল্টি-প্ল্যাটফর্ম অ্যাপ্লিকেশন তৈরি করার জন্য Google-এর একটি ওপেন সোর্স ফ্রেমওয়ার্ক৷   Flutter হল একটি ওপেন সোর্স সফটওয়্যার ডেভেলপমেন্ট কিট যা  সহজ ক্রস-প্ল্যাটফর্ম মোবাইল অ্যাপ ডেভেলপমেন্ট সক্ষম করে। আপনি ios এবং অ্যান্ড্রয়েডের জন্য উচ্চ মানের নেটিভলি কম্পাইল করা অ্যাপ তৈরি করতে পারেন, দুটি অ্যাপের জন্য আলাদাভাবে কোড না লিখেই। উভয় প্ল্যাটফর্মের জন্য আপনার যা দরকার তা হল একটি কোডবেস`,
+        image: 'https://scontent.fcla5-1.fna.fbcdn.net/v/t39.30808-6/358637189_1782564398865564_3971267723525124330_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeGYfib4baudh7jyFhlmFVPLsNKxR2wDgeqw0rFHbAOB6pXW03tAXT_c9G2OFH21qy0NGKQ_DjdFmObL-xgmOm1-&_nc_ohc=ZLx5V9AgNhgAX-nQnu_&_nc_zt=23&_nc_ht=scontent.fcla5-1.fna&oh=00_AfD0bdMYiK3Kla89OWggSSAu45B8ElmMgUqx7qpe4vioSQ&oe=64ACBB7D',
+        time: "28 min",
+        like: '44',
+        comment: '3',
+        author: {
+            name: 'Hablu Programmer Community',
+            image: "https://scontent.fcla5-1.fna.fbcdn.net/v/t39.30808-6/219454213_206089904788271_2880338216491903517_n.jpg?stp=c19.0.50.50a_cp0_dst-jpg_p50x50&_nc_cat=101&ccb=1-7&_nc_sid=70495d&_nc_eui2=AeEEpYJhgRgO9QNzu-1gkao1Y3Zt9mNp-Etjdm32Y2n4S1YjlM8aH5Sp9GZmXcJkDk31BtUl-VR0JozRqNLEiVKh&_nc_ohc=YkEQodX0CS0AX-WRC1k&_nc_zt=23&_nc_ht=scontent.fcla5-1.fna&oh=00_AfBfYN0KZFIsKmFG6ZsX1CWpjW2_wgVCCx3hc8cmwoRJnQ&oe=64AD9CB9"
+        }
+    },
+    {
+        id: 8,
+        content: `যদিও আজ ব্যাট হাতে ব্যার্থ হয়েছেন, বল হাতে দুর্দান্ত সাকিব, হাতের ইঞ্জুরি হয়তো এখনো পুরোপুরি ঠিক হয়নি, তারপরও নিজের সেরাটা দিচ্ছেন 👊
+        এজন্যই তিনি সাকিব, তিনি বিশ্বসেরা 👑`,
+        image: 'https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-6/358413105_975573800353044_1089360555061978516_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeHh2tFJIJzblt4PccmNFPVS5Fst08Jj1OPkWy3TwmPU4-bUxlVeGE-b89reamucrYBfEkmFSzZ0nPLOTCJUDHkI&_nc_ohc=zxGRDLmv45AAX81Ac0l&_nc_zt=23&_nc_ht=scontent.fcla5-2.fna&oh=00_AfBP81la1YH6fclAlQzmk1CbKBdGE9ZknWa7EjzPUquPyg&oe=64AD8B8C',
+        time: "23 min",
+        like: '444',
+        comment: '33',
+        author: {
+            name: 'SOUMYA SARKAR AND LITTON DAS FANS ',
+            image: "https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-6/342530245_1422228935267576_7518872458885679228_n.jpg?stp=c20.0.50.50a_cp0_dst-jpg_p50x50&_nc_cat=1&ccb=1-7&_nc_sid=70495d&_nc_eui2=AeGVzt_WDpad5Sa2O6-lXX6TLxnYo2BcPswvGdijYFw-zOOxFNReF6p3X22SRz5cBXD_q3P3ocNhvqqHVWks6zgV&_nc_ohc=abJjO92oGpQAX_w4DXU&_nc_zt=23&_nc_ht=scontent.fcla5-2.fna&oh=00_AfDKvRdnZq_Q0q9azk-APrFdpO8sKkU0hw6VdqyBaEbWAg&oe=64ACB87F"
+        }
+    },
+    {
+        id: 9,
+        content: `‘তামিমের এমন কথা আমার মেজাজ খারাপ করে দিয়েছে। এটা কি সে পাড়া-মহল্লার খেলা পেয়ে গেছে নাকি! যে আমি ফিট আছি কি না, তা খেলে দেখব। হয় আপনি ফিট, নয়তো ফিট না। এটা তো পেশাদার কোনো আচরণ হতে পারে না। আন্তর্জাতিক ম্যাচ কি এমন মনোভাব নিয়ে কেউ খেলতে নামে? এটা হয় নাকি!’
+        ‘আমি কোচের সঙ্গে কথা বলেছি। কোচও এমন কোনো কিছুর সঙ্গে অভ্যস্ত নয়। সেও ক্ষুব্ধ।কিছুক্ষণ আগে ফো`,
+        image: 'https://scontent.fcla5-1.fna.fbcdn.net/v/t39.30808-6/358434694_679676744177171_116733363558961984_n.jpg?stp=dst-jpg_p180x540&_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeF3tidPpe0lVwQGzTpc5q7q9GcnHuZWSX_0Zyce5lZJf28ARZa1gs_gLhsfv0cerhjaHWf5OOMhrbQYbS_V3Ts5&_nc_ohc=zM6KdHucsFkAX_sLWrW&_nc_zt=23&_nc_ht=scontent.fcla5-1.fna&oh=00_AfB1Shhz-zNEvCjOyDl8eZqP3OJtDA9v9fTBhiOshKRsYg&oe=64AC5452',
+        time: "20 min",
+        like: '500',
+        comment: '20',
+        author: {
+            name: 'Daily Bangla Bazaar',
+            image: "https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-1/242438449_372805177728447_5723491154859259169_n.jpg?stp=cp0_dst-jpg_p40x40&_nc_cat=1&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeHroScWnXExiFT6jBkJ_4ZNeOTyFx-NXkV45PIXH41eRfHRqC0WogNbR5q7BVIoTVT1j9jE3R_eOtudiQ4f8vJ-&_nc_ohc=5op9UppKZXIAX9zcWDJ&_nc_ht=scontent.fcla5-2.fna&oh=00_AfDYR8PU08m0d1usi9GDdJrH2NmrPbOsQEpSc9wQprZYjw&oe=64AD2CB7"
+        }
+    },
+    {
+        id: 10,
+        content: `kacci biriyani.`,
+        image: 'https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-6/358541862_574030964940406_7621056325682319054_n.jpg?stp=dst-jpg_s640x640&_nc_cat=1&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGGQaeMcCzCp9_FLrMzD5rfPjPgJV5HSUw-M-AlXkdJTKhhRvHD7npa1VPexE-xtXjfH7rRtbc9y_eWmkb-feJd&_nc_ohc=qi7vk_gkwaYAX_Jcutu&_nc_zt=23&_nc_ht=scontent.fcla5-2.fna&oh=00_AfD3SOkNZDfpqC6W9G-T9j5bgwBz_mb2dtVeBwSCgBZWJA&oe=64ACAAE4',
+        time: "10 min",
+        like: '665',
+        comment: '443',
+        author: {
+            name: 'Sultans Dine',
+            image: "https://scontent.fcla5-2.fna.fbcdn.net/v/t39.30808-1/310360581_400447582298746_7155187679035987573_n.jpg?stp=cp0_dst-jpg_p40x40&_nc_cat=1&ccb=1-7&_nc_sid=c6021c&_nc_eui2=AeEdvcIK-kdqe5oq5HywvKM5eQbs5U-cKJZ5BuzlT5wolv3n6lfbCEjSTfXhpoyC3XFU-CIVOCxSxoy2Gogm6ERl&_nc_ohc=gaiLQRHYslwAX_bKLGt&_nc_ht=scontent.fcla5-2.fna&oh=00_AfBgKKK608_HI2y-QXvMNDwuiJ2RZtL2axKtgnHT0Hl9Mw&oe=64ADDA52"
+        }
+    },
+]
